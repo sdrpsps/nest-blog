146258d9002a9f5aeea03c2c73e000f9993110bc
sdrpsps <z826700@gmail.com>
2022-10-14 10:27:18 +0800

    jwt模块

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { hash } from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
+import LoginDto from './dto/login.dto';
 import RegisterDto from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {
+    constructor(private prisma: PrismaService, private jwt: JwtService) {
 
     }
     // 注册
@@ -16,13 +18,26 @@ export class AuthService {
                 password: await hash(dto.password)
             }
         })
-
         const res = {
             success: true,
             code: 201,
             message: "注册成功"
         }
-
         return res
+    }
+
+    // 登录
+    async login(dto: LoginDto) {
+
+    }
+
+    // 生成 Jwt Token
+    async token({ name, id }) {
+        return {
+            token: await this.jwt.signAsync({
+                name,
+                sub: id
+            })
+        }
     }
 }
